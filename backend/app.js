30df8a23499510679dc0277b4bbd502dcdccc57c
Danijel Tomić <danijeltomic2004@gmail.com>
2025-05-28 10:31:48 +0200

    Add dockerzation to whole project and docker compose script for easy deploy

--- backend/app.js
+++ backend/app.js
@@ -1,82 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var mongoose = require('mongoose');
-var mongoDB = "mongodb://127.0.0.1/PametniPaketnik";
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+require("dotenv").config(); // Load .env if present
+const { db, mongoURI } = require("./db"); // This sets up the connection
 
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/userRoutes');
-var tokenRouter = require('./routes/tokenRoutes');
-var lockboxRouter = require('./routes/lockboxRoutes');
-var transactionController = require('./routes/transactionRoutes');
+var indexRouter = require("./routes/index");
+var userRouter = require("./routes/userRoutes");
+var tokenRouter = require("./routes/tokenRoutes");
+var lockboxRouter = require("./routes/lockboxRoutes");
+var transactionController = require("./routes/transactionRoutes");
 
 var app = express();
 
-var cors = require('cors');
-var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-app.use(cors({
-  credentials: true,
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin)===-1){
-      var msg = "The CORS policy does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+var cors = require("cors");
+var allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
-app.use(session({
-  secret: 'work hard',
-  resave: true,
-  saveUninitialized: false,
-  store: MongoStore.create({mongoUrl: mongoDB})
-}));
+var session = require("express-session");
+var MongoStore = require("connect-mongo");
+app.use(
+  session({
+    secret: "work hard",
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: mongoURI }),
+  })
+);
 
 app.use(function (req, res, next) {
   res.locals.session = req.session;
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/api/user', userRouter);
-app.use('/api/token', tokenRouter);
-app.use('/api/lockbox', lockboxRouter);
-app.use('/api/transaction', transactionController);
+app.use("/api/", indexRouter);
+app.use("/api/user", userRouter);
+app.use("/api/token", tokenRouter);
+app.use("/api/lockbox", lockboxRouter);
+app.use("/api/transaction", transactionController);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,70 +1,106 @@
-import React from 'react';
-import { Box, Button, TextField, Typography, Divider, Paper, Link, Alert } from '@mui/material';
-import FaceIcon from '@mui/icons-material/Face';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { useState, useContext } from 'react';
-import { UserContext } from '../UserContext'; 
-function Login(){
+import React from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Divider,
+  Paper,
+  Link,
+  Alert,
+} from "@mui/material";
+import FaceIcon from "@mui/icons-material/Face";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { UserContext } from "../UserContext";
+function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const userContext = useContext(UserContext); 
+  const userContext = useContext(UserContext);
 
-async function Login(e){
-        e.preventDefault();
-        if(username === "" || password === ""){
-            setUsername("");
-            setPassword("");
-            setError("Invalid username or password");
-            return;
-        }
-        const res = await fetch("http://localhost:3001/api/user/login", {
-            method: "POST",
-            credentials: "include",
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        });
-        const data = await res.json();
-        if(data._id !== undefined){
-            userContext.setUserContext(data);
-            navigate("/");
-            
-        } else {
-            setUsername("");
-            setPassword("");
-            setError("Invalid username or password");
-        }
+  async function Login(e) {
+    e.preventDefault();
+    if (username === "" || password === "") {
+      setUsername("");
+      setPassword("");
+      setError("Invalid username or password");
+      return;
     }
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/api/user/login`, {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const data = await res.json();
+    if (data._id !== undefined) {
+      userContext.setUserContext(data);
+      navigate("/");
+    } else {
+      setUsername("");
+      setPassword("");
+      setError("Invalid username or password");
+    }
+  }
 
   return (
-    <Paper elevation={3} sx={{ p: 4, maxWidth: 400, mx: 'auto', mt: 6 }}>
+    <Paper elevation={3} sx={{ p: 4, maxWidth: 400, mx: "auto", mt: 6 }}>
       <Typography variant="h5" align="center" gutterBottom>
         Login
       </Typography>
       <Box component="form" noValidate autoComplete="off" onSubmit={Login}>
-        <TextField label="Username" fullWidth margin="normal" value={username} onChange={(e)=>(setUsername(e.target.value))}/>
-        <TextField label="Password" type="password" fullWidth margin="normal" value={password} onChange={(e)=>(setPassword(e.target.value))}/>
+        <TextField
+          label="Username"
+          fullWidth
+          margin="normal"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         {error && <Alert severity="error">{error}</Alert>}
-        <Button variant="contained" color="primary" fullWidth sx={{ mt: 2 }} type='submit'>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ mt: 2 }}
+          type="submit"
+        >
           Login
         </Button>
         <Divider sx={{ my: 2 }}>OR</Divider>
-        <Button variant="outlined" startIcon={<FaceIcon />} fullWidth sx={{ mt: 1 }}>
+        <Button
+          variant="outlined"
+          startIcon={<FaceIcon />}
+          fullWidth
+          sx={{ mt: 1 }}
+        >
           FaceID
         </Button>
         <Typography sx={{ mt: 2 }} align="center">
           {"Don't have an account? "}
-          <Link component="button" variant="body2" onClick={() => navigate('/register')}>
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => navigate("/register")}
+          >
             Sign up instead
           </Link>
         </Typography>
       </Box>
     </Paper>
   );
-};
+}
 
 export default Login;
